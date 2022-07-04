ced5a9f016b2178f22d4078395e8e165c978cec2
Connor Mackay <connor.mackay@gmail.com>
2022-07-04 18:30:49 -0400

    remove unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../Components/Header/Header";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Connor Mackay</title>
         <meta name="description" content="Connor Mackay's personal site" />
