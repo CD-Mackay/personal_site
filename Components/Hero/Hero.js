129186c9ced6b877408df13266597897beb5af92
Connor Mackay <connor.mackay@gmail.com>
2022-07-05 13:02:18 -0400

    style hero background

--- Components/Hero/Hero.js
+++ Components/Hero/Hero.js
@@ -3,7 +3,9 @@ import styles from "./Hero.module.css";
 export default function Hero() {
   return (
     <div className={styles.hero}>
-      I am Hero
+      <div className={styles.text}>
+        Hi there! Im Connor and I make websites!
+      </div>
     </div>
   );
 }
