6b4c73d5a1c7d5b1c4e89c16ba48a85c28da008b
Connor Mackay <connor.mackay@gmail.com>
2022-07-05 13:21:35 -0400

    Add project tech to cards

--- Components/ProjectCard/ProjectCard.js
+++ Components/ProjectCard/ProjectCard.js
@@ -1,9 +1,14 @@
 import styles from './ProjectCard.module.css';
 
-export default function ProjectCard({name}) {
+export default function ProjectCard({name, tech}) {
   return (
     <div className={styles.card}>
       <h6>{name}</h6>
+      <ul>
+        {tech.map((element, index) => {
+          return <li key={index}>{element}</li>
+        })}
+      </ul>
     </div>
   )
 };
--- Components/Projects/Projects.js
+++ Components/Projects/Projects.js
@@ -5,10 +5,10 @@ export default function Projects() {
   return (
     <div>
       <h3>Projects</h3>
-      <ProjectCard name={"Staffify"} />
-      <ProjectCard name={"Notes App"} />
-      <ProjectCard name={"HelpMeHelpYou"} />
-      <ProjectCard name={"Mini Arcade"} />
+      <ProjectCard name={"Staffify"} tech={["ReactJs"]}/>
+      <ProjectCard name={"Notes App"} tech={["ReactJS", "DraftJS", "Axios", "Express", "Postgresql"]} />
+      <ProjectCard name={"HelpMeHelpYou"} tech={["ReactJS", "Axios", "Rails", "Postgresql", "Socket.io"]} />
+      <ProjectCard name={"Mini Arcade"}  tech={["ReactJS"]}/>
     </div>
   )
 };
