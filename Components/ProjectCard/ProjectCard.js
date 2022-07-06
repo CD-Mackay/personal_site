6e5786d08f7da1067deed9c8cebf518e3d646b19
Connor Mackay <connor.mackay@gmail.com>
2022-07-06 18:24:33 -0400

    add images to project sections

--- Components/ProjectCard/ProjectCard.js
+++ Components/ProjectCard/ProjectCard.js
@@ -1,14 +1,21 @@
-import styles from './ProjectCard.module.css';
+import styles from "./ProjectCard.module.css";
+import Image from "next/image";
 
-export default function ProjectCard({name, tech}) {
+export default function ProjectCard({ name, tech }) {
+  
   return (
     <div className={styles.card}>
+      <Image
+        src="https://github.com/CD-Mackay/personal_site/blob/main/public/mini_arcade_screen.png"
+        alt="application screenshot"
+        layout="fill"
+      />
       <h6>{name}</h6>
       <ul>
         {tech.map((element, index) => {
-          return <li key={index}>{element}</li>
+          return <li key={index}>{element}</li>;
         })}
       </ul>
     </div>
-  )
-};
+  );
+}
