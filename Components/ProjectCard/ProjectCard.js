ec2628f0e3bf0c383cd60a137910c6e8aedb1583
Connor Mackay <connor.mackay@gmail.com>
2022-07-07 09:56:18 -0400

    add react- icons

--- Components/ProjectCard/ProjectCard.js
+++ Components/ProjectCard/ProjectCard.js
@@ -1,7 +1,9 @@
 import styles from "./ProjectCard.module.css";
 import Image from "next/image";
+import Link from "next/link";
+import { BsGithub } from 'react-icons/bs';
 
-export default function ProjectCard({ name, tech }) {
+export default function ProjectCard({ name, tech, url }) {
   
   return (
     <div className={styles.card}>
@@ -10,7 +12,7 @@ export default function ProjectCard({ name, tech }) {
         alt="application screenshot"
         layout="intrinsic"
         width={400}
-        height={400}
+        height={200}
       />
       <h6>{name}</h6>
       <ul>
@@ -18,6 +20,8 @@ export default function ProjectCard({ name, tech }) {
           return <li key={index}>{element}</li>;
         })}
       </ul>
+      <Link href={url} target="_blank">
+      </Link>
     </div>
   );
 }
--- Components/Projects/Projects.js
+++ Components/Projects/Projects.js
@@ -6,16 +6,26 @@ export default function Projects() {
     <div className={styles.projects}>
       <h3>Projects</h3>
       <div className={styles.grid}>
-        <ProjectCard name={"Staffify"} tech={["ReactJs"]} />
+        <ProjectCard
+          name={"Staffify"}
+          tech={["ReactJs"]}
+          url="https://github.com/CD-Mackay/staffing-app"
+        />
         <ProjectCard
           name={"Notes App"}
           tech={["ReactJS", "DraftJS", "Axios", "Express", "Postgresql"]}
+          url="https://github.com/CD-Mackay/notes-app"
         />
         <ProjectCard
           name={"HelpMeHelpYou"}
           tech={["ReactJS", "Axios", "Rails", "Postgresql", "Socket.io"]}
+          url="https://github.com/alan-mak/HelpMeHelpYou"
         />
-        <ProjectCard name={"Mini Arcade"} tech={["ReactJS"]} />
+        <ProjectCard
+          name={"Mini Arcade"}
+          tech={["ReactJS"]}
+          url="https://github.com/CD-Mackay/mini-arcade"
+        />
       </div>
     </div>
   );
