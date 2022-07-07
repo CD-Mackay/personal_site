e0ffe727216457e1635f72c3a5b5ca73dee492e1
Connor Mackay <connor.mackay@gmail.com>
2022-07-07 13:34:15 -0400

    style projectcard layout

--- Components/ProjectCard/ProjectCard.js
+++ Components/ProjectCard/ProjectCard.js
@@ -3,18 +3,18 @@ import Image from "next/image";
 import Link from "next/link";
 import { BsGithub } from "react-icons/bs";
 
-export default function ProjectCard({ name, tech, url }) {
+export default function ProjectCard({ name, tech, url, image }) {
   return (
     <div className={styles.card}>
       <div>
         <Image
-          src="/staffing_app_screen.png"
+          src={image}
           alt="application screenshot"
           layout="intrinsic"
           width={400}
           height={200}
         />
-        <h6>{name}</h6>
+        <h6 className={styles.header}>{name}</h6>
         <ul>
           {tech.map((element, index) => {
             return <li key={index}>{element}</li>;
