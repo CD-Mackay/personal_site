6d62a63017910f1dc617c9cc3e6ad5e300a4d93e
Connor Mackay <connor.mackay@gmail.com>
2022-07-08 09:24:41 -0400

    add contact info

--- Components/ResumeDoc/ResumeDoc.js
+++ Components/ResumeDoc/ResumeDoc.js
@@ -1,10 +1,20 @@
+import Link from "next/link";
 import styles from "./ResumeDoc.module.css";
-
 
 export default function ResumeDoc() {
   return (
     <div className={styles.page}>
-      <div className={styles.contact}>The blue side</div>
+      <div className={styles.contact}>
+        <h2>Connor Mackay</h2>
+        <h6>Toronto Web Developer</h6>
+        <p>Contact</p>
+        <p>647-393-4597</p>
+        <p>connor.mackay@gmail.com</p>
+        <div className={styles.links}>
+          <Link href="">Github: CD-Mackay</Link>
+          <Link href="">Linkedin: Connor Mackay</Link>
+        </div>
+      </div>
       <div className={styles.experience}>The white side</div>
     </div>
   );
