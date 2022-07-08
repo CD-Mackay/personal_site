f5a60546b4aee6430ce4312f31acc69f01645146
Connor Mackay <connor.mackay@gmail.com>
2022-07-08 09:21:04 -0400

    add styling

--- Components/ResumeDoc/ResumeDoc.js
+++ Components/ResumeDoc/ResumeDoc.js
@@ -4,8 +4,8 @@ import styles from "./ResumeDoc.module.css";
 export default function ResumeDoc() {
   return (
     <div className={styles.page}>
-      <div>The blue side</div>
-      <div>The white side</div>
+      <div className={styles.contact}>The blue side</div>
+      <div className={styles.experience}>The white side</div>
     </div>
   );
 }
