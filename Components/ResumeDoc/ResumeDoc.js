96ac7708b7580c2a0cdac88d22a66a273d34ff60
Connor Mackay <connor.mackay@gmail.com>
2022-07-08 09:26:27 -0400

    transcribe skills/experience

--- Components/ResumeDoc/ResumeDoc.js
+++ Components/ResumeDoc/ResumeDoc.js
@@ -15,7 +15,20 @@ export default function ResumeDoc() {
           <Link href="">Linkedin: Connor Mackay</Link>
         </div>
       </div>
-      <div className={styles.experience}>The white side</div>
+      <div className={styles.experience}>
+        <h3>Profile</h3>
+        <p>
+          A graduate from Lighthouse Labs Development program looking to turn a
+          passion for technology into a long-term career. I am an enthusiastic,
+          fast-learning team player seeking an entry level technology role
+          supporting the development, testing and deployment of dynamic
+          front-end interfaces for clients.
+        </p>
+        <h3>Skills</h3>
+        <h3>Education</h3>
+        <h3>Work Experience</h3>
+        
+      </div>
     </div>
   );
 }
