5ac179083bb7eb7570c3e4e293b8bf8562fafe2c
Connor Mackay <connor.mackay@gmail.com>
2022-07-08 09:38:56 -0400

    add projects section

--- Components/ResumeDoc/ResumeDoc.js
+++ Components/ResumeDoc/ResumeDoc.js
@@ -5,14 +5,19 @@ export default function ResumeDoc() {
   return (
     <div className={styles.page}>
       <div className={styles.contact}>
-        <h2>Connor Mackay</h2>
-        <h6>Toronto Web Developer</h6>
-        <p>Contact</p>
-        <p>647-393-4597</p>
-        <p>connor.mackay@gmail.com</p>
-        <div className={styles.links}>
-          <Link href="">Github: CD-Mackay</Link>
-          <Link href="">Linkedin: Connor Mackay</Link>
+        <div>
+          <h2>Connor Mackay</h2>
+          <h6>Toronto Web Developer</h6>
+          <p>Contact</p>
+          <p>647-393-4597</p>
+          <p>connor.mackay@gmail.com</p>
+          <div className={styles.links}>
+            <Link href="">Github: CD-Mackay</Link>
+            <Link href="">Linkedin: Connor Mackay</Link>
+          </div>
+        </div>
+        <div>
+          <h6>Projects</h6>
         </div>
       </div>
       <div className={styles.experience}>
