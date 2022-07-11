d7b2a0274a264c6810240cc455ca7a123ff90e9b
Connor Mackay <connor.mackay@gmail.com>
2022-07-10 20:35:17 -0400

    add metadata to resume page

--- pages/resume.js
+++ pages/resume.js
@@ -1,10 +1,16 @@
 import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
-import ResumeDoc from '../Components/ResumeDoc/ResumeDoc';
+import ResumeDoc from "../Components/ResumeDoc/ResumeDoc";
+import Head from "next/head";
 
 export default function Resume() {
   return (
     <div>
+      <Head>
+        <title>Connor Mackay Resume</title>
+        <meta name="description" content="Connor Mackay's Resume" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <ResumeDoc />
     </div>
