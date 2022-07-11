cb3598ee5a10ce746c52ee5c5bb5e9b00bbcb1de
Connor Mackay <connor.mackay@gmail.com>
2022-07-11 11:58:56 -0400

    add header, refactor skills layout

--- Components/Skills/Skills.js
+++ Components/Skills/Skills.js
@@ -4,51 +4,54 @@ import styles from "./Skills.module.css";
 export default function Skills() {
   return (
     <div className={styles.skills}>
-      <div className={styles.list}>
-        <h5>Languages</h5>
-        <ul>
-          <li>Javascript</li>
-          <li>Ruby</li>
-          <li>HTML5</li>
-          <li>CSS3</li>
-        </ul>
-      </div>
-      <div className={styles.list}>
-        <h5>Libraries & Frameworks</h5>
-        <ul>
-        <li>ReactJS</li>
-        <li>Ruby on Rails</li>
-        <li>NextJS</li>
-        <li>Jquery</li>
-        <li>Ajax</li>
-        <li>ExpressJs</li>
-        </ul>
-      </div>
-      <div className={styles.list}>
-        <h5>Testing</h5>
-        <ul>
-          <li>Mocha/Chai</li>
-          <li>Jest</li>
-          <li>Cypress</li>
-          <li>Storybook</li>
-        </ul>
-      </div>
-      <div className={styles.list}>
-        <h5>Database Systems</h5>
-        <ul>
-          <li>SQL</li>
-          <li>PostgreSQL</li>
-          <li>GraphQL</li>
-        </ul>
-      </div>
-      <div className={styles.list}>
-        <h5>Source & Project Management</h5>
-        <ul>
-          <li>Github</li>
-          <li>GitKraken</li>
-          <li>Jira</li>
-          <li>Jenkins</li>
-        </ul>
+      <h3>Skills</h3>
+      <div className={styles.skills_grid}>
+        <div className={styles.list}>
+          <h5>Languages</h5>
+          <ul>
+            <li>Javascript</li>
+            <li>Ruby</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
+          </ul>
+        </div>
+        <div className={styles.list}>
+          <h5>Libraries & Frameworks</h5>
+          <ul>
+            <li>ReactJS</li>
+            <li>Ruby on Rails</li>
+            <li>NextJS</li>
+            <li>Jquery</li>
+            <li>Ajax</li>
+            <li>ExpressJs</li>
+          </ul>
+        </div>
+        <div className={styles.list}>
+          <h5>Testing</h5>
+          <ul>
+            <li>Mocha/Chai</li>
+            <li>Jest</li>
+            <li>Cypress</li>
+            <li>Storybook</li>
+          </ul>
+        </div>
+        <div className={styles.list}>
+          <h5>Database Systems</h5>
+          <ul>
+            <li>SQL</li>
+            <li>PostgreSQL</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
+        <div className={styles.list}>
+          <h5>Source & Project Management</h5>
+          <ul>
+            <li>Github</li>
+            <li>GitKraken</li>
+            <li>Jira</li>
+            <li>Jenkins</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
