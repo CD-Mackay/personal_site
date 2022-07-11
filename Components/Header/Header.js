32578067f70e8f2f297832d9d7a242ad540a63b9
Connor Mackay <connor.mackay@gmail.com>
2022-07-10 20:20:09 -0400

    add conditional render to header content

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,20 +1,28 @@
 import Link from "next/link";
 import styles from "./Header.module.css";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const router = useRouter();
+
+  console.log(router.pathname);
+
   return (
     <div className={styles.header}>
-        <div className={styles.header_content}>
-          <p>To save as pdf:   1. Right click, click &lsquo;Print&lsquo;   2. Change Destination to &lsquo;Save as PDF&lsquo;   3. Press Save</p>
-          </div>
-        <nav className={styles.navigation}>
-          <div className={styles.link}>
-            <Link href="/">Home</Link>
-          </div>
-          <div className={styles.link}>
-            <Link href="/resume">Resume</Link>
-          </div>
-        </nav>
+      <div className={styles.header_content}>
+        {router.pathname === "/resume" && <p>
+          To save as pdf: 1. Right click, click &lsquo;Print&lsquo; 2. Change
+          Destination to &lsquo;Save as PDF&lsquo; 3. Press Save
+        </p>}
+      </div>
+      <nav className={styles.navigation}>
+        <div className={styles.link}>
+          <Link href="/">Home</Link>
+        </div>
+        <div className={styles.link}>
+          <Link href="/resume">Resume</Link>
+        </div>
+      </nav>
     </div>
   );
 }
