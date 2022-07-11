399aa6eb7d977d19b0a6c8d8136a00eb8e646ea9
Connor Mackay <connor.mackay@gmail.com>
2022-07-10 20:18:15 -0400

    update header content, style header layout

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -4,15 +4,17 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <div className={styles.header}>
-      <div>I am Header</div>
-      <nav className={styles.navigation}>
-        <div className={styles.link}>
-          <Link href="/">Home</Link>
-        </div>
-        <div className={styles.link}>
-          <Link href="/resume">Resume</Link>
-        </div>
-      </nav>
+        <div className={styles.header_content}>
+          <p>To save as pdf:   1. Right click, click &lsquo;Print&lsquo;   2. Change Destination to &lsquo;Save as PDF&lsquo;   3. Press Save</p>
+          </div>
+        <nav className={styles.navigation}>
+          <div className={styles.link}>
+            <Link href="/">Home</Link>
+          </div>
+          <div className={styles.link}>
+            <Link href="/resume">Resume</Link>
+          </div>
+        </nav>
     </div>
   );
 }
