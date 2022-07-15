48d9b2d9d800a0628e7ed54ec0ee56b274e83915
Connor Mackay <connor.mackay@gmail.com>
2022-07-15 15:23:39 -0400

    refactor hero layout, add lorem ipsum

--- Components/Hero/Hero.js
+++ Components/Hero/Hero.js
@@ -4,9 +4,19 @@ export default function Hero() {
   return (
     <div className={styles.hero}>
       <div className={styles.text}>
-        <h1>Connor Mackay</h1>
+        <h1>Connor Mackay&#39;s Site</h1>
         <p>Toronto based Web Developer</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
       </div>
+      <div />
     </div>
   );
 }
