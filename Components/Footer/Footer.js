6e51ed4c779d839f075310dd0e323d96ff715f72
Connor Mackay <connor.mackay@gmail.com>
2022-07-15 16:12:19 -0400

    convert anchors to icons

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -1,14 +1,15 @@
 import styles from "./Footer.module.css";
 import Link from "next/link";
+import { AiFillGithub, AiFillLinkedin} from 'react-icons/ai';
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.content}>
         <p className={styles.footer_text}>Connor Mackay</p>
-        <Link href="https://github.com/CD-Mackay">Github</Link>
+        <Link href="https://github.com/CD-Mackay"><AiFillGithub fontSize="24px" /></Link>
         <Link href="https://www.linkedin.com/in/connor-mackay-800992bb/">
-          LinkedIn
+          <AiFillLinkedin fontSize="24px" />
         </Link>
       </div>
     </div>
