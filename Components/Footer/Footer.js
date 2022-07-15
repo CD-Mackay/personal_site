17c38e878023556b8081f51089201db6bfee1e0b
Connor Mackay <connor.mackay@gmail.com>
2022-07-15 16:10:15 -0400

    style footer layout, add anchors

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -1,7 +1,16 @@
-import styles from './Footer.module.css';
+import styles from "./Footer.module.css";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className={styles.footer}>I am Footer</div>
-  )
+    <div className={styles.footer}>
+      <div className={styles.content}>
+        <p className={styles.footer_text}>Connor Mackay</p>
+        <Link href="https://github.com/CD-Mackay">Github</Link>
+        <Link href="https://www.linkedin.com/in/connor-mackay-800992bb/">
+          LinkedIn
+        </Link>
+      </div>
+    </div>
+  );
 }
