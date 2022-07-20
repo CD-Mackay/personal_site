2307a4f78c15129958abc03378ff601f35ffa1de
Connor Mackay <connor.mackay@gmail.com>
2022-07-20 11:28:33 -0400

    refactor skills component to use mapped data

--- Components/Skills/Skills.js
+++ Components/Skills/Skills.js
@@ -1,59 +1,40 @@
 import ProjectCard from "../ProjectCard/ProjectCard";
+import SkillsList from "../SkillsList/SkillsList";
 import styles from "./Skills.module.css";
 
 export default function Skills() {
 
   //// MAKE MINI COMPONENTS FOR EACH SUBHEADING W/LIST AND MAP OUT AN OBJECT DUUUUH
+
+  const SKILLS_DATA = [
+    {
+      heading: "Languages",
+      skills: ["Javascript", "Ruby", "HTML5", "CSS3"]
+    },
+    {
+      heading: "Libraries & Frameworks",
+      skills: ["ReactJS", "Ruby on Rails", "NextJS", "Jquery", "Ajax", "ExpressJs"]
+    },
+    {
+      heading: "Testing",
+      skills: ["Mocha/Chai", "Jest", "Cypress", "Storybook"]
+    },
+    {
+      heading: "Database Systems",
+      skills: ["SQL", "PostgreSQL", "GraphQL"]
+    },
+    {
+      heading: "Source & Project Management",
+      skills: ["Github", "GitKraken", "Jira", "Jenkins"]
+    },
+  ]
   return (
     <div className={styles.skills}>
       <h2 className={styles.skills_header}>Skills</h2>
       <div className={styles.skills_grid}>
-        <div className={styles.list}>
-          <h3 className={styles.skill_header}>Languages</h3>
-          <ul>
-            <li>Javascript</li>
-            <li>Ruby</li>
-            <li>HTML5</li>
-            <li>CSS3</li>
-          </ul>
-        </div>
-        <div className={styles.list}>
-          <h3 className={styles.skill_header}>Libraries & Frameworks</h3>
-          <ul>
-            <li>ReactJS</li>
-            <li>Ruby on Rails</li>
-            <li>NextJS</li>
-            <li>Jquery</li>
-            <li>Ajax</li>
-            <li>ExpressJs</li>
-          </ul>
-        </div>
-        <div className={styles.list}>
-          <h3 className={styles.skill_header}>Testing</h3>
-          <ul>
-            <li>Mocha/Chai</li>
-            <li>Jest</li>
-            <li>Cypress</li>
-            <li>Storybook</li>
-          </ul>
-        </div>
-        <div className={styles.list}>
-          <h3 className={styles.skill_header}>Database Systems</h3>
-          <ul>
-            <li>SQL</li>
-            <li>PostgreSQL</li>
-            <li>GraphQL</li>
-          </ul>
-        </div>
-        <div className={styles.list}>
-          <h3 className={styles.skill_header}>Source & Project Management</h3>
-          <ul>
-            <li>Github</li>
-            <li>GitKraken</li>
-            <li>Jira</li>
-            <li>Jenkins</li>
-          </ul>
-        </div>
+        {SKILLS_DATA.map((skill, index) => {
+          return <SkillsList heading={skill.heading} key={index} skills={skill.skills} />
+        })}
       </div>
     </div>
   );
---
+++ Components/SkillsList/SkillsList.js
@@ -0,0 +1,17 @@
+import ProjectCard from "../ProjectCard/ProjectCard";
+import Skills from "../Skills/Skills";
+import styles from "./SkillsList.module.css";
+
+export default function SkillsList({heading, skills}) {
+
+  return (
+        <div className={styles.list}>
+          <h3 className={styles.skill_header}>{heading}</h3>
+          <ul>
+            {skills.map((skill, index) => {
+              return <li key={index}>{skill}</li>
+            })}
+          </ul>
+        </div>
+  );
+}
