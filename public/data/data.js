cac194144de36213a668046e0e8b0ca8efc717e5
Connor Mackay <connor.mackay@gmail.com>
2022-07-20 11:37:52 -0400

    abstract out data

---
+++ public/data/data.js
@@ -0,0 +1,64 @@
+export const PROJECT_DATA = [
+  {
+    name: "Staffify",
+    tech: "ReactJs",
+    description:
+      "A human resources app for managing employees and teams. Employees can be assigned to projects, flagged for performance and affiliated with technical skills.",
+    url: "https://github.com/CD-Mackay/staffing-app",
+    image: "/staffing_app_screen.png",
+  },
+  {
+    name: "Notes App",
+    tech: "ReactJS, DraftJS, Axios, Express, PostgreSQL",
+    description:
+      "Intended as a practice project to expand my knowledge of React libraries, the Notes App uses DraftJS to allow rich text-editing. Notes can be categorized and searched for based on tags.",
+    url: "https://github.com/CD-Mackay/notes-app",
+    image: "/notes_app_screen.png",
+  },
+  {
+    name: "Mini Arcade",
+    tech: "ReactJS",
+    description:
+      "Working with React in a way I am unfamiliar with. Inspired by a practice tic-tac-toe project I made, the concept was expanded into a series of mini-games",
+    url: "https://github.com/CD-Mackay/mini-arcade",
+    image: "/mini_arcade_screen.png",
+  },
+  {
+    name: "HelpMeHelpYou",
+    tech: "ReactJs, Axios, Rails, PostgreSQL, Socket.io",
+    description:
+      "Collaborated with Alan Mak and Justin Chan. HelpMeHelpYou is a community based app, allowing helpful neighbors to connect with their neighbors in need. Features a websocket based chat room.",
+    url: "https://github.com/alan-mak/HelpMeHelpYou",
+    image: "/helpmehelpyou.png",
+  },
+];
+
+export const SKILLS_DATA = [
+  {
+    heading: "Languages",
+    skills: ["Javascript", "Ruby", "HTML5", "CSS3"],
+  },
+  {
+    heading: "Libraries & Frameworks",
+    skills: [
+      "ReactJS",
+      "Ruby on Rails",
+      "NextJS",
+      "Jquery",
+      "Ajax",
+      "ExpressJs",
+    ],
+  },
+  {
+    heading: "Testing",
+    skills: ["Mocha/Chai", "Jest", "Cypress", "Storybook"],
+  },
+  {
+    heading: "Database Systems",
+    skills: ["SQL", "PostgreSQL", "GraphQL"],
+  },
+  {
+    heading: "Source & Project Management",
+    skills: ["Github", "GitKraken", "Jira", "Jenkins"],
+  },
+];
