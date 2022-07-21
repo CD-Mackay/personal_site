f41930284af929e3e0e4e9b6998b680ace892ffa
Connor Mackay <connor.mackay@gmail.com>
2022-07-21 13:30:59 -0400

    add hero text content

--- Components/Hero/Hero.js
+++ Components/Hero/Hero.js
@@ -5,15 +5,19 @@ export default function Hero() {
     <div className={styles.hero}>
       <div className={styles.text}>
         <h1>Connor Mackay&#39;s Site</h1>
-        <p>Toronto based Web Developer</p>
+        {/* <p>Toronto based Web Developer</p> */}
         <p className={styles.hero_body}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
+          I am a front-end developer specializing in ReactJs and similar
+          front-end technology. I have experience working with teams of a wide
+          variety of disciplines and skills levels, and believe I am able to
+          contribute in any professional environment. 
+        </p>
+        <p className={styles.hero_body}>
+          I have always had a lifelong fascination with how technology interacts
+          with and shapes our way of experiencing the world. This
+          curiosity has led me to a career in web development, where I hope to
+          put my passion and curiosity to use laying down the next stages of
+          online infrastructure.
         </p>
       </div>
       <div />
