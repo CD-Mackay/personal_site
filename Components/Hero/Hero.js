9db97ee88f2e49fa52c4ba22aa6eed04ee5fad9d
Connor Mackay <connor.mackay@gmail.com>
2022-07-22 11:26:45 -0400

    edit hero copy

--- Components/Hero/Hero.js
+++ Components/Hero/Hero.js
@@ -6,17 +6,16 @@ export default function Hero() {
       <div className={styles.text}>
         <h1>Connor Mackay&#39;s Site</h1>
         <p className={styles.hero_body}>
-          I am a front-end developer specializing in ReactJs and similar
-          front-end technology. I have experience working with teams of a wide
-          variety of disciplines and skills levels, and believe I am able to
-          contribute in any professional environment.
+          I am a front-end developer specializing in ReactJs and other front-end
+          technology. I have experience working with teams of a wide variety of
+          disciplines and skills levels, and believe I am able to contribute in
+          any professional environment.
         </p>
         <p className={styles.hero_body}>
-          I have always had a lifelong fascination with how technology interacts
-          with and shapes our way of experiencing the world. This curiosity has
-          led me to a career in web development, where I hope to put my passion
-          and curiosity to use laying down the next stages of online
-          infrastructure.
+          I have always had a lifelong fascination with how technology shapes
+          our way of experiencing the world. This curiosity has led me to a
+          career in web development, where I hope to put my passion and
+          curiosity to use laying down the next stages of online infrastructure.
         </p>
       </div>
       <div />
