3efa01de4c58736c096e0ee31cd998868de63e61
Connor Mackay <connor.mackay@gmail.com>
2022-07-26 17:23:53 -0400

    import footer to page, style layout

--- pages/tictactoe.js
+++ pages/tictactoe.js
@@ -1,17 +1,19 @@
 import Header from "../Components/Header/Header";
 import Head from "next/head";
 import Game from "../Components/Game/Game";
+import Footer from '../Components/Footer/Footer';
 
 export default function Resume() {
   return (
     <div>
       <Head>
-        <title>Connor Mackay Resume</title>
-        <meta name="description" content="Connor Mackay's Resume" />
+        <title>Tic Tac Toe</title>
+        <meta name="description" content="Connor Mackay's portfolio, sample of game portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <Game />
+      <Footer />
     </div>
   );
 }
