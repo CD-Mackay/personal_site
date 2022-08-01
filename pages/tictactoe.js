335fac4a497adeabf11f851ed493fa804d5ecfa2
Connor Mackay <connor.mackay@gmail.com>
2022-08-01 14:54:07 -0400

    refactor game to use context

--- pages/tictactoe.js
+++ pages/tictactoe.js
@@ -1,19 +1,25 @@
 import Header from "../Components/Header/Header";
 import Head from "next/head";
 import Game from "../Components/Game/Game";
-import Footer from '../Components/Footer/Footer';
+import Footer from "../Components/Footer/Footer";
+import { GameContextProvider } from "../Components/Context/GameContext";
 
 export default function Resume() {
   return (
-    <div>
-      <Head>
-        <title>Tic Tac Toe</title>
-        <meta name="description" content="Connor Mackay's portfolio, sample of game portfolio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <Game />
-      <Footer />
-    </div>
+    <GameContextProvider>
+      <div>
+        <Head>
+          <title>Tic Tac Toe</title>
+          <meta
+            name="description"
+            content="Connor Mackay's portfolio, sample of game portfolio"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <Game />
+        <Footer />
+      </div>
+    </GameContextProvider>
   );
 }
